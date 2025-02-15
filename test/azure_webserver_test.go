4ddb12021efea6b574802811fb976339f3d72e46
Rhythm Sharma <sharmarhythm89@gmail.com>
2025-02-14 19:11:09 -0500

    Added additional tests for NIC and Ubuntu version

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -8,15 +8,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// You normally want to run this under a separate "Testing" subscription
-// For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
+// Subscription ID for Azure
 var subscriptionID string = "06926fb4-9ce6-411d-9ff8-1bdec3ad355c"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
-		// The path to where our Terraform code is located
 		TerraformDir: "../",
-		// Override the default terraform variables
 		Vars: map[string]interface{}{
 			"labelPrefix": "shar0855",
 		},
@@ -24,13 +21,55 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 
 	defer terraform.Destroy(t, terraformOptions)
 
-	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the value of output variable
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+}
+
+func TestNetworkInterfaceExists(t *testing.T) {
+	t.Parallel()
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../",
+	}
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	nicID := terraform.Output(t, terraformOptions, "nic_id")
+	vmID := terraform.Output(t, terraformOptions, "vm_id")
+
+	// Test that the NIC exists
+	nicExists := azure.NetworkInterfaceExists(t, nicID, subscriptionID)
+	assert.True(t, nicExists, "Network Interface should exist")
+
+	// Check if NIC is connected to the VM
+	vmNicConnection := azure.CheckNICConnection(t, vmID, nicID, subscriptionID)
+	assert.True(t, vmNicConnection, "NIC should be connected to the VM")
+
+	terraform.Destroy(t, terraformOptions)
+}
+
+func TestUbuntuVersion(t *testing.T) {
+	t.Parallel()
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../",
+	}
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	publicIP := terraform.Output(t, terraformOptions, "public_ip")
+
+	sshUser := "azureuser"
+	sshKeyPath := "/path/to/your/ssh/private/key"
+	versionCheckCmd := "lsb_release -a"
+	output := terraform.RunSSHCommand(t, publicIP, sshUser, sshKeyPath, versionCheckCmd)
+
+	assert.Contains(t, output, "Ubuntu 20.04", "VM should be running Ubuntu 20.04")
+
+	terraform.Destroy(t, terraformOptions)
 }
